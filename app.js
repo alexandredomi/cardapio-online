083d22c52ba0f0ba0cbcc47833a3d836ea831e82
Alexandre Domingues <149201812+alexandredomi@users.noreply.github.com>
2025-01-17 14:27:08 -0300

    add

---
+++ app.js
@@ -0,0 +1,40 @@
+var btnAbrirCard = document.querySelector('#btnAbrirCard')
+var containerModalCard = document.querySelector('.container-modal-card')
+
+btnAbrirCard.addEventListener('click', ()=>{
+      containerModalCard.classList.toggle('ativo')
+})
+btnAbrirCardSair.addEventListener('click', ()=>{
+      containerModalCard.classList.toggle('ativo')
+})
+
+//abri menu
+var btnAbrirMenu = document.querySelector('#btnAbrirMenu')
+var btnAbrirMenuSair = document.querySelector('#btnAbrirMenuSair')
+var containerMenu = document.querySelector('.container-menu')
+
+btnAbrirMenu.addEventListener('click', ()=>{
+      containerMenu.classList.toggle('ativo')
+})
+btnAbrirMenuSair.addEventListener('click', ()=>{
+      containerMenu.classList.toggle('ativo')
+})
+
+//consumo json
+var listaCardapio = document.querySelector('.lista-cardapio')
+
+fetch('/data.json')
+      .then(response => response.json())
+      .then(json => console.log(json.bebidas))
+      
+//alerta enviar pedido
+var btnEnviarPedido = document.querySelector('#enviar-pedido')
+
+      btnEnviarPedido.addEventListener('click', ()=>{
+            let confirmarenvio = confirm('deseja enviar pedido ao resourante')
+            if(confirmarenvio){
+                  alert('Pedido enviado com sucesso')
+            }else{
+                  alert('O pedido nao foi enviado')
+            }
+      })
